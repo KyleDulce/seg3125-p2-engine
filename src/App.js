c8c26b53b9d0bae5548b3a0e9586102f311cdf7b
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2022-06-28 15:11:30 -0400

    fixed imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Learn from './pages/Learn';
